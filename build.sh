d9d65816cdc3df3786d4cd451c2a6b1ba96e2bf4
Tomasz Zdybał <tomek@zdybal.lap.pl>
2019-05-15 21:53:01 +0200

    build.sh: small bugfixes
    Fixed:
     - `ethereumproject` added to paths
     - `GOCACHE` removed, because of Go 1.12 compatibility issues
     - relative path from build.sh to vendor, for simplicity

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-pushd $GOPATH/src/github.com/ethereum/go-ethereum/vendor/github.com/evm-ffi/c/
+pushd ./vendor/github.com/ethereumproject/evm-ffi/c/
 make
 popd
 
 mkdir -p build/bin
-GOCACHE=off CGO_LDFLAGS="$GOPATH/src/github.com/ethereum/go-ethereum/vendor/github.com/evm-ffi/c/libsputnikvm.a -ldl -lm" go build -o build/bin/geth ./cmd/geth
+CGO_LDFLAGS="$GOPATH/src/github.com/ethereum/go-ethereum/vendor/github.com/ethereumproject/evm-ffi/c/libsputnikvm.a -ldl -lm" go build -o build/bin/geth ./cmd/geth
 
