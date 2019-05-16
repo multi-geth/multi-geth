44a3dd4a08e3e0d13b74dbe7ebf95959fc8e5cf0
meows <b5c6@protonmail.com>
2019-05-16 09:25:13 -0500

    params: add multigeth-specific changes
    Current through multigeth history at
    960f53f3809b31aac654c5f9831545c3c3785c85

--- params/gas_table.go
+++ params/gas_table.go
@@ -63,9 +63,10 @@ var (
 
 		CreateBySuicide: 25000,
 	}
-	// GasTableEIP158 contain the gas re-prices for
-	// the EIP155/EIP158 phase.
-	GasTableEIP158 = GasTable{
+
+	// GasTableEIP160 contain the gas re-prices for
+	// the EIP160 phase.
+	GasTableEIP160 = GasTable{
 		ExtcodeSize: 700,
 		ExtcodeCopy: 700,
 		Balance:     400,
@@ -76,9 +77,10 @@ var (
 
 		CreateBySuicide: 25000,
 	}
-	// GasTableConstantinople contain the gas re-prices for
+
+	// GasTableEIP1052 contain the gas re-prices for
 	// the constantinople phase.
-	GasTableConstantinople = GasTable{
+	GasTableEIP1052 = GasTable{
 		ExtcodeSize: 700,
 		ExtcodeCopy: 700,
 		ExtcodeHash: 400,
