82ac8948865ac1541a2463eedc29ce047e493839
meows <b5c6@protonmail.com>
2019-05-16 09:47:58 -0500

    consensus/ethash: fix typo

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -526,11 +526,6 @@ func (ethash *Ethash) Prepare(chain consensus.ChainReader, header *types.Header)
 // Finalize implements consensus.Engine, accumulating the block and uncle rewards,
 // setting the final state and assembling the block.
 func (ethash *Ethash) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
-// setting the final state on the header
-func (ethash *Ethash) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header) {
-	header.Root = state.IntermediateRoot(chain.Config().IsEIP161F(header.Number))
-}
-
 	// Accumulate any block and uncle rewards and commit the final state root
 	accumulateRewards(chain.Config(), state, header, uncles)
 	header.Root = state.IntermediateRoot(chain.Config().IsEIP161F(header.Number))
