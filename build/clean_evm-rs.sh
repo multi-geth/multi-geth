6f8aa405c27ed1ef311692b2361848199606cdd4
meows <b5c6@protonmail.com>
2019-05-20 12:04:54 -0500

    build/clean_evm-rs.sh: nitpickery

--- build/clean_evm-rs.sh
+++ build/clean_evm-rs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Cleaning the evm-rs build only makes sense if we actually have cargo installe
-if ! command -v cargo > /dev/null; then
-  exit
+# Cleaning the evm-rs build only makes sense if we actually have cargo installed
+if ! command -v cargo 2>&1 /dev/null; then
+  exit 1
 fi
 
 make -C vendor/github.com/ethereumproject/evm-ffi/c/ clean
