8c70abfd8440ed498e2006e0338e0ac46ef65168
Mike Lubinets <public@mersinvald.me>
2019-05-21 17:55:22 +0300

    fix: remove test_with_svm.sh as it's misleading: it won't run tests with SVM as current test harness doesn't support choosing the evm.

--- test_with_svm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-make -C ./vendor/github.com/ethereumproject/evm-ffi/c/
-
-mkdir -p build/bin
-CGO_LDFLAGS="$GOPATH/src/github.com/ethereum/go-ethereum/vendor/github.com/ethereumproject/evm-ffi/c/libsputnikvm.a -ldl -lm" go test ./tests -v
-
