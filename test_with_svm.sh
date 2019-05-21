6a8e5ef654b3cc0aa5517bd5443db3be604e9b77
Mike Lubinets <public@mersinvald.me>
2019-05-21 17:52:21 +0300

    fix(scripts): pushd/popd -> simple make

--- test_with_svm.sh
+++ test_with_svm.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-pushd ./vendor/github.com/ethereumproject/evm-ffi/c/
-make
-popd
+make -C ./vendor/github.com/ethereumproject/evm-ffi/c/
 
 mkdir -p build/bin
 CGO_LDFLAGS="$GOPATH/src/github.com/ethereum/go-ethereum/vendor/github.com/ethereumproject/evm-ffi/c/libsputnikvm.a -ldl -lm" go test ./tests -v
