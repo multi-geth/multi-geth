7e52b4fa5fb9db4ca42b3f158ab55d017df6b056
meows <b5c6@protonmail.com>
2019-06-03 14:56:05 -0500

    core,tests: implement use of parity chainspec for test chainconfigs
    Failing:
    
    $ go test -run TestBlockchain ./tests/...
    --- FAIL: TestBlockchain (0.02s)
        --- FAIL: TestBlockchain/TransitionTests/bcEIP158ToByzantium/ByzantiumTransition.json (0.17s)
            block_test.go:49: Block #5 insertion into chain failed: invalid merkle root (remote: fa8bb175564fa6e8b8990d582fd009c465a5be4688d6f3dd7d540a76fff9169f local: 9806bb0ae31fc8c581a668ce3570c7ac2a0f07eaec5b3b6a554165be5ae66145 161enabled:true)
    FAIL
    FAIL    github.com/ethereum/go-ethereum/tests   13.008s

--- tests/init.go
+++ tests/init.go
@@ -17,64 +17,102 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/big"
+	"os"
+	"path/filepath"
 
+	xchainparity "github.com/etclabscore/eth-x-chainspec/parity"
 	"github.com/ethereum/go-ethereum/params"
 )
 
+var chainspecsDir = filepath.Join(os.Getenv("GOPATH"), "src/github.com/ethereum/go-ethereum/tests/chainspecs")
+
+func mustReadChainspec(specFilename string) *params.ChainConfig {
+	chainspecFile := filepath.Join(chainspecsDir, specFilename)
+	b, err := ioutil.ReadFile(chainspecFile)
+	if err != nil {
+		panic("read config file " + chainspecFile)
+	}
+
+	pc := xchainparity.Config{}
+	err = json.Unmarshal(b, &pc)
+	if err != nil {
+		panic("unmarshal chainspec")
+	}
+
+	mggen := pc.ToMultiGethGenesis()
+	if mggen == nil {
+		panic("nil genesis converted")
+	}
+	return mggen.Config
+}
+
 // Forks table defines supported forks and their chain config.
 var Forks = map[string]*params.ChainConfig{
-	"Frontier": {
-		ChainID: big.NewInt(1),
-	},
-	"Homestead": {
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-	},
-	"EIP150": {
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-	},
-	"EIP158": {
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-	},
-	"Byzantium": {
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-		DAOForkBlock:   big.NewInt(0),
-		ByzantiumBlock: big.NewInt(0),
-	},
-	"Constantinople": {
-		ChainID:             big.NewInt(1),
-		HomesteadBlock:      big.NewInt(0),
-		EIP150Block:         big.NewInt(0),
-		EIP155Block:         big.NewInt(0),
-		EIP158Block:         big.NewInt(0),
-		DAOForkBlock:        big.NewInt(0),
-		ByzantiumBlock:      big.NewInt(0),
-		ConstantinopleBlock: big.NewInt(0),
-		PetersburgBlock:     big.NewInt(10000000),
-	},
-	"ConstantinopleFix": {
-		ChainID:             big.NewInt(1),
-		HomesteadBlock:      big.NewInt(0),
-		EIP150Block:         big.NewInt(0),
-		EIP155Block:         big.NewInt(0),
-		EIP158Block:         big.NewInt(0),
-		DAOForkBlock:        big.NewInt(0),
-		ByzantiumBlock:      big.NewInt(0),
-		ConstantinopleBlock: big.NewInt(0),
-		PetersburgBlock:     big.NewInt(0),
-	},
+	// https://github.com/paritytech/parity-ethereum/blob/1871275ecdf02431bf67d09a1b25be8ff8916e3a/ethcore/src/client/evm_test_client.rs#L98
+	// https://github.com/paritytech/parity-ethereum/blob/0199acbece836c49e07410796c40c185e9051451/ethcore/src/ethereum/mod.rs#L129
+	"Frontier":             mustReadChainspec("frontier_test.json"),
+	"Homestead":            mustReadChainspec("homestead_test.json"),
+	"EIP150":               mustReadChainspec("eip150_test.json"),
+	"EIP158":               mustReadChainspec("eip161_test.json"),
+	"Byzantium":            mustReadChainspec("byzantium_test.json"),
+	"Constantinople":       mustReadChainspec("constantinople_test.json"),
+	"ConstantinopleFix":    mustReadChainspec("st_peters_test.json"),
+	"EIP158ToByzantiumAt5": mustReadChainspec("transition_test.json"),
+
+	// "Frontier": {
+	// 	ChainID: big.NewInt(1),
+	// },
+	// "Homestead": {
+	// 	ChainID:        big.NewInt(1),
+	// 	HomesteadBlock: big.NewInt(0),
+	// },
+	// "EIP150": {
+	// 	ChainID:        big.NewInt(1),
+	// 	HomesteadBlock: big.NewInt(0),
+	// 	EIP150Block:    big.NewInt(0),
+	// },
+	// "EIP158": {
+	// 	ChainID:        big.NewInt(1),
+	// 	HomesteadBlock: big.NewInt(0),
+	// 	EIP150Block:    big.NewInt(0),
+	// 	EIP155Block:    big.NewInt(0),
+	// 	EIP158Block:    big.NewInt(0),
+	// },
+	// "Byzantium": {
+	// 	ChainID:        big.NewInt(1),
+	// 	HomesteadBlock: big.NewInt(0),
+	// 	EIP150Block:    big.NewInt(0),
+	// 	EIP155Block:    big.NewInt(0),
+	// 	EIP158Block:    big.NewInt(0),
+	// 	DAOForkBlock:   big.NewInt(0),
+	// 	ByzantiumBlock: big.NewInt(0),
+	// },
+	// "Constantinople": {
+	// 	ChainID:             big.NewInt(1),
+	// 	HomesteadBlock:      big.NewInt(0),
+	// 	EIP150Block:         big.NewInt(0),
+	// 	EIP155Block:         big.NewInt(0),
+	// 	EIP158Block:         big.NewInt(0),
+	// 	DAOForkBlock:        big.NewInt(0),
+	// 	ByzantiumBlock:      big.NewInt(0),
+	// 	ConstantinopleBlock: big.NewInt(0),
+	// 	PetersburgBlock:     big.NewInt(10000000),
+	// },
+	// "ConstantinopleFix": {
+	// 	ChainID:             big.NewInt(1),
+	// 	HomesteadBlock:      big.NewInt(0),
+	// 	EIP150Block:         big.NewInt(0),
+	// 	EIP155Block:         big.NewInt(0),
+	// 	EIP158Block:         big.NewInt(0),
+	// 	DAOForkBlock:        big.NewInt(0),
+	// 	ByzantiumBlock:      big.NewInt(0),
+	// 	ConstantinopleBlock: big.NewInt(0),
+	// 	PetersburgBlock:     big.NewInt(0),
+	// },
 	"FrontierToHomesteadAt5": {
 		ChainID:        big.NewInt(1),
 		HomesteadBlock: big.NewInt(5),
@@ -90,14 +128,14 @@ var Forks = map[string]*params.ChainConfig{
 		DAOForkBlock:   big.NewInt(5),
 		DAOForkSupport: true,
 	},
-	"EIP158ToByzantiumAt5": {
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: big.NewInt(0),
-		EIP150Block:    big.NewInt(0),
-		EIP155Block:    big.NewInt(0),
-		EIP158Block:    big.NewInt(0),
-		ByzantiumBlock: big.NewInt(5),
-	},
+	// "EIP158ToByzantiumAt5": {
+	// 	ChainID:        big.NewInt(1),
+	// 	HomesteadBlock: big.NewInt(0),
+	// 	EIP150Block:    big.NewInt(0),
+	// 	EIP155Block:    big.NewInt(0),
+	// 	EIP158Block:    big.NewInt(0),
+	// 	ByzantiumBlock: big.NewInt(5),
+	// },
 	"ByzantiumToConstantinopleAt5": {
 		ChainID:             big.NewInt(1),
 		HomesteadBlock:      big.NewInt(0),
