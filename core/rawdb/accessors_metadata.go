65858a044d2d03af5cf95beaf692c3605ec1597d
meows <b5c6@protonmail.com>
2019-06-03 13:38:00 -0500

    core/rawdb: remove debugging line

--- core/rawdb/accessors_metadata.go
+++ core/rawdb/accessors_metadata.go
@@ -60,7 +60,6 @@ func ReadChainConfig(db ethdb.Reader, hash common.Hash) *params.ChainConfig {
 	}
 	var config params.ChainConfig
 	if err := json.Unmarshal(data, &config); err != nil {
-		panic("gotcha2")
 		log.Error("Invalid chain config JSON", "hash", hash, "err", err)
 		return nil
 	}
